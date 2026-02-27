d19aa7655214ffc26563b2b1e9d07f53e55bf64e
Harsh Jain <jnharsh464@gmail.com>
2026-02-27 18:43:36 +0530

    Update Server.js

--- Server/Server.js
+++ Server/Server.js
@@ -32,7 +32,7 @@ const path = require("path");
 app.use(express.static(path.join(__dirname, "client", "build")));
 
 // Catch-all route (SPA support)
-app.get("*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
