b7b8867766e68e51aa4f2d4ec302d53faab94330
Harsh Jain <jnharsh464@gmail.com>
2026-03-03 16:22:47 +0530

    Update Nodemailer.js

--- Server/helpers/Nodemailer.js
+++ Server/helpers/Nodemailer.js
@@ -2,13 +2,27 @@
 const nodemailer = require("nodemailer");
 
 const sendMail = async (to, subject, html) => {
+  // const transporter = nodemailer.createTransport({
+  //   service: "gmail",
+  //   auth: {
+  //     user: process.env.MAIL_ID,
+  //     pass: process.env.MAIL_PASSWORD,
+  //   },
+  // });
+
   const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.MAIL_ID,
-      pass: process.env.MAIL_PASSWORD,
-    },
-  });
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.MAIL_ID,
+    pass: process.env.MAIL_PASSWORD,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+  connectionTimeout: 15000,
+});
 
   const mailOptions = {
     from: process.env.MAIL_ID,
